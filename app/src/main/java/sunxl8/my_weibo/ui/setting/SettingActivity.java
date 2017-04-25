e94e061a16cbbb6e786326229c3908ee3d4e103c
sunxiaolei <xiaoleisun92@gmail.com>
2017-04-25 11:28:50 +0800

    清除缓存

--- app/src/main/java/sunxl8/my_weibo/ui/setting/SettingActivity.java
+++ app/src/main/java/sunxl8/my_weibo/ui/setting/SettingActivity.java
@@ -2,7 +2,9 @@ package sunxl8.my_weibo.ui.setting;
 
 import android.content.Intent;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.jakewharton.rxbinding.view.RxView;
 import com.trello.rxlifecycle.android.ActivityEvent;
 
@@ -10,6 +12,8 @@ import butterknife.BindView;
 import sunxl8.my_weibo.R;
 import sunxl8.my_weibo.ui.base.BaseSwipeActivity;
 import sunxl8.my_weibo.ui.base.IPresenter;
+import sunxl8.myutils.CleanUtils;
+import sunxl8.myutils.FileUtils;
 
 /**
  * Created by sunxl8 on 2017/4/10.
@@ -19,6 +23,10 @@ public class SettingActivity extends BaseSwipeActivity {
 
     @BindView(R.id.layout_setting_account_manage)
     RelativeLayout layoutAccountManage;
+    @BindView(R.id.layout_setting_clean_cache)
+    RelativeLayout layoutCleanCache;
+    @BindView(R.id.tv_setting_cache)
+    TextView tvCache;
 
     @Override
     protected IPresenter createPresenter() {
@@ -42,6 +50,17 @@ public class SettingActivity extends BaseSwipeActivity {
                 .compose(this.bindUntilEvent(ActivityEvent.DESTROY))
                 .subscribe(aVoid -> {
                     startActivity(new Intent(SettingActivity.this, AccountManagerActivity.class));
+                });
+        tvCache.setText(FileUtils.getDirSize(Glide.getPhotoCacheDir(this).getPath()));
+        RxView.clicks(layoutCleanCache)
+                .compose(this.bindUntilEvent(ActivityEvent.DESTROY))
+                .subscribe(aVoid -> {
+                    showDialog(getString(R.string.sure_clean_cache), (dialog, which) -> {
+                        CleanUtils.cleanExternalCache();
+                        CleanUtils.cleanInternalCache();
+                        showToast(getString(R.string.clean_cache_already));
+                        tvCache.setText(FileUtils.getDirSize(Glide.getPhotoCacheDir(this).getPath()));
+                    });
                 });
     }
 
